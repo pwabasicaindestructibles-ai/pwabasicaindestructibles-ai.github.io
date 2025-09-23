f7fe40697133806f2ab8a522f0847ade36256f4a
pwabasicaindestructibles-ai <pwabasicaindestructibles@gmail.com>
2025-09-22 20:09:48 -0600

    sw-register.js

--- sw-register.js
+++ sw-register.js
@@ -1,7 +1,8 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('service-worker.js')
       .then(reg => console.log('SW registrado:', reg.scope))
       .catch(err => console.error('Error registrando SW:', err));
   });
 }
+
